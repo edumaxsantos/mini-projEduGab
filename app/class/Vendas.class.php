7d6b0d028737d4ea7331fb19c0eb747470201755
Gabriel Santos <gabmaxsan@gmail.com>
2016-12-26 01:30:43 -0200

    Nova classe vendas

--- app/class/Vendas.class.php
+++ app/class/Vendas.class.php
@@ -2,19 +2,19 @@
 
 class Vendas extends Produtos{
 	private $data_venda;
-	private $produtos_vendidos;
+	private $lista;
 	private $preco_total;
 
-	public __construct() {
-		$this->produtos_vendidos = [];
+	public function __construct() {
+		$this->lista = [];
 	}
 
-	public function setProdutosVendidos($produto) {
-		$this->produtos_vendidos[] = $produto;
+	public function setLista($lista) {
+		$this->lista[] = $lista;
 	}
 
-	public function getProdutosVendidos() {
-		return $this->produtos_vendidos;
+	public function getLista() {
+		return $this->lista;
 	}
 
 	public function getDataVenda() {
@@ -23,13 +23,6 @@ class Vendas extends Produtos{
 
     public function setDataVenda($data_venda) {
         $this->data_venda = $data_venda;
-    }
-
-    public function getQtdVenda() {
-    	return $this->qtd_venda;
-    }
-    public function setQtdVenda($qtd_venda) {
-    	$this->qtd_venda = $qtd_venda;
     }
 
     public function getPrecoTotal() {
