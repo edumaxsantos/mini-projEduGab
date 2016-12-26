33876affd9994483f85b27c479470f7eadb2acf6
Eduardo Santos <edumaxsantos@gmail.com>
2016-12-26 04:38:24 -0200

    consegue converter o json para objeto php

--- app/inserirVenda.php
+++ app/inserirVenda.php
@@ -3,20 +3,25 @@
 function __autoload ($class) {
 	require_once("class/".$class.".class.php");
 }
-	 
+
 // cria objetos
 $venda = new Vendas();
 $vendaSql = new SqlVendas();
 
 //pegar com post
-$lista = json_decode(addslashes($_POST['lista']));
-
+$lista = addslashes($_POST['lista']);
+var_dump($lista);
+$lista = str_replace("\\", "", $lista);
+var_dump($lista);
+$lista =json_decode($lista);
+var_dump($lista);
+var_dump($lista->{'1'});
 //calcula preco final
 $num = count($lista);
 $x = 0;
 $pf = 0;
 while($x > $num){
-	$vendas->Buscar($lista[$x][0]);
+	$vendasSql->Buscar($lista[$x][0]);
 	$p = $vendas->getPreco();
 	$pf .= $p*$lista[$x][1];
 	$x++;
@@ -28,7 +33,7 @@ $venda->setDataVenda(date('Y-m-d'));
 $venda->setPrecoTotal($pf);
 
 //Envia para BD e retorna para outra pagina
-$return = $vendaSql->InserirV();
-$teste = (substr($return, 0 ,8) ==  "SQLSTATE") ? ("<script> alert('Erro na compra, tente novamente');  history.back();</script>") : ("<script> alert('Compra realizada com sucesso!');  history.back();</script>");
+$return = $vendaSql->InserirV($venda);
+$teste = (substr($return, 0 ,8) ==  "SQLSTATE") ? ("<script> alert('Erro na compra, tente novamente');  </script>") : ("<script> alert('Compra realizada com sucesso!');</script>");
 echo $teste;
 ?>
