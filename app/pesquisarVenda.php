a29cd312b284229883e4b6d9b057baed14653d98
Gabriel Santos <gabmaxsan@gmail.com>
2016-12-27 01:44:25 -0200

    pesquiser venda adicionado

---
+++ app/pesquisarVenda.php
@@ -0,0 +1,27 @@
+<?php
+function __autoload ($class) {
+ require_once("class/".$class.".class.php");
+ }
+header('Content-Type: text/html; charset=utf-8');
+
+//Cria objetos
+$vendas = new Vendas();
+$vendasSql = new SqlVendas();
+
+//Pegar com POST
+$id = addslashes($_REQUEST['id']);
+
+//Faz o select e mostra valores
+$buscar = $vendasSql->BuscarV($id);
+if($buscar){
+	$ar = (array)$buscar;
+	$json = json_encode($ar);
+	$json = str_replace('\u0000', '', $json);
+	$json = str_replace('*', '', $json);
+	$json = str_replace('\\', '', $json);
+	echo $json;
+}
+else{
+	$json = "sem linha";
+	echo json_encode($json);
+}
